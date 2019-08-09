cc13ea1b39a07e5050072545680b44fb3a47bf4d
flatsiedatsie <unfold@gmail.com>
2019-08-09 14:37:11 +0000

    0.0.1

--- bootstrap.py
+++ bootstrap.py
@@ -61,7 +61,7 @@ try:
     #import fuzzywuzzy  # noqa: F401
     #import alsaaudio  # noqa: F401
     #import dateutil # noqa: F401
-    import python-vlc # noqa: F401
+    import vlc # noqa: F401
 except ImportError as ex:
     print("Import error: " + str(ex))
     # If installation failed, exit with 100 to tell the gateway not to restart
--- install_vlc.sh
+++ install_vlc.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Installing VLC player..."
 
-sudo apt-get install vlc
+sudo apt-get install vlc -y
 
 echo "VLC player should now be installed"
