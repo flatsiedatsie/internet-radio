2a58244ccdaa4b5d3bf9ad797db5e6ee75882553
Michael Stegeman <michael@stegeman.me>
2020-03-18 11:16:46 -0800

    Add macOS support.
    Also, clean up a bit.

--- package.sh
+++ package.sh
@@ -1,29 +1,30 @@
-#!/bin/bash
+#!/bin/bash -e
 
 version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
 
-
 # Clean up from previous releases
-rm -rf *.tgz package
-rm -f SHA256SUMS
-rm -rf lib
+rm -rf *.tgz package SHA256SUMS lib
 
-# Put package together
-mkdir package
-mkdir lib
+# Prep new package
+mkdir lib package
 
 # Pull down Python dependencies
 pip3 install -r requirements.txt -t lib --no-binary requests --prefix ""
 
-
-cp *.py *.json requirements.txt setup.cfg LICENSE README.md package/
-cp -r lib pkg package/
+# Put package together
+cp -r lib pkg LICENSE manifest.json package.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
 find package -type d -empty -delete
 
+# Generate checksums
 cd package
-find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
-cd ..
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
 
-tar czf "internet-radio-${version}.tgz" package
-sha256sum "internet-radio-${version}.tgz"
+# Make the tarball
+TARFILE="internet-radio-${version}.tgz"
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
