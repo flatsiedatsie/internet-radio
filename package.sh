c744099afc465658b9bd53ec71d6ad6597f05e28
flatsiedatsie <unfold@gmail.com>
2020-08-02 07:59:30 +0200

    1.1.2

--- package.sh
+++ package.sh
@@ -1,30 +1,33 @@
 #!/bin/bash -e
 
-version=$(grep '"version":' manifest.json | cut -d: -f2 | cut -d\" -f2)
-
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
 # Clean up from previous releases
-rm -rf *.tgz package SHA256SUMS
+rm -rf *.tgz *.sha256sum package SHA256SUMS
 
 # Prep new package
 mkdir package
 
 # Put package together
-cp -r pkg LICENSE package.json manifest.json *.py README.md package/
+cp -r pkg LICENSE manifest.json *.py README.md package/
 find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
 find package -type d -empty -delete
 
+echo "generating checksums"
 cd package
-find . -type f \! -name SHA256SUMS -exec sha256sum {} \; >> SHA256SUMS
-cd ..
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
 
-# Make the tarball
-#TARFILE="internet-radio-${version}.tgz"
-#tar czf ${TARFILE} package
+echo "creating archive"
+TARFILE="internet-radio-${version}.tgz"
+tar czf ${TARFILE} package
 
-#shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
 
 #rm -rf SHA256SUMS package
 #sha256sum "internet-radio-${version}.tgz"
 
 tar czf "internet-radio-${version}.tgz" package
-sha256sum "internet-radio-${version}.tgz"
+cat ${TARFILE}.sha256sum
+#sha256sum "internet-radio-${version}.tgz"
