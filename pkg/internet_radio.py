37f7c29b9cc39fc8b09bea1f7c93cc5622a21771
flatsiedatsie <unfold@gmail.com>
2020-12-08 15:23:20 +0100

    1.1.6
    automation

--- pkg/internet_radio.py
+++ pkg/internet_radio.py
@@ -6,7 +6,7 @@
 
 import os
 import sys
-sys.path.append(path.join(path.dirname(path.abspath(__file__)), 'lib'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'lib'))
 import re
 import subprocess
 import sys
