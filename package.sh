039c68bbdca06231523ff7298d76d8e5e69902e4
flatsiedatsie <unfold@gmail.com>
2020-12-08 15:19:05 +0100

    1.1.6

--- package.sh
+++ package.sh
@@ -1,8 +1,20 @@
 #!/bin/bash -e
 
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
+# Setup environment for building inside Dockerized toolchain
+[ $(id -u) = 0 ] && umask 0
+
 # Clean up from previous releases
 rm -rf *.tgz *.sha256sum package SHA256SUMS
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
+
 
 # Prep new package
 mkdir package
